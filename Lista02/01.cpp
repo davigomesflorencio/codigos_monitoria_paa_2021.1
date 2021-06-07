4b02f763a8a9fcb5df57bc4f35074fadc5182681
davigomesflorencio <davigomesflorencio@gmail.com>
2021-06-07 19:04:12 -0300

    Alteraçao na Questao 1 da Lista 02

--- Lista02/01.cpp
+++ Lista02/01.cpp
@@ -13,15 +13,17 @@ int bruteforce(vector<int> V , int X){
   int aux = MAX_VALUE;
 	for(int i=0;i < V.size();i++){
 		if(V[i]<=X){
-			aux = min(aux,1 + bruteforce(V,X-V[i]));
+      vector<int> ut = V;
+      ut.erase(ut.begin()+i);
+			aux = min(aux,1 + bruteforce(ut,X-V[i]));
 		}
 	}
 	return aux;
 }
 
-int main(int argc, char const *argv[]) {
-  vector<int> V = {50, 20, 20, 2, 10 ,10,10,10,5};
-  int X=45;
+int main() {
+  vector<int> V = {50, 20, 20,5,2};
+  int X=49;
   
   int f =bruteforce(V,X); 
   cout<< f<<endl; 	
